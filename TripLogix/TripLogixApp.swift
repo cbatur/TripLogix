d55b5bc4e16c5e5bdb8caee4daff1ea54aa75177
Cantek Batur <batur@batur.ca>
2024-03-23 00:09:41 -0400

    Initial Commit

---
+++ TripLogix/TripLogixApp.swift
@@ -0,0 +1,17 @@
+//
+//  TripLogixApp.swift
+//  TripLogix
+//
+//  Created by Cantek Batur on 2024-03-23.
+//
+
+import SwiftUI
+
+@main
+struct TripLogixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
